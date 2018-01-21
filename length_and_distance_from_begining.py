bd34a782d2d6906ed84863c5a3e4fdab95b2b51d
Amitesh Arora <amitesharora1@gmail.com>
2018-01-20 16:42:52 -0800

    Just added the description to the module and a function that was missing it.

--- length_and_distance_from_begining.py
+++ length_and_distance_from_begining.py
@@ -39,6 +39,5 @@ def points_by_length_and_distance_from_begining(text:TextBlob):
 
 if __name__ == '__main__':
     #For testing
-    print(length_tuples(TextBlob('I am Amitesh. This is a summarization project.')))
     print(repeating_words(TextBlob('This is a check for redundant words and redundant phrases phrases. Being redundant is is redundant. This is not a repeating sentence')))
     print(redundant_words(TextBlob('This is absolutely and completely neccessary. It was a very unintentional phenonmenon')))
